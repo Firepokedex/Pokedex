5e6113d7f047da68750f5b1dbfacae955b077078
allen <allen1999@hotmail.ca>
2025-02-25 00:47:58 -0500

    Added stats for Pokemon

--- pokedex.js
+++ pokedex.js
@@ -1,6 +1,6 @@
 
 const pokemonCount = 251;
-var pokedex = {}; // {1: {"name": "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "....", "shinyImg": url}}
+var pokedex = {}; // {1: {"name": "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "....", "shinyImg": url, "stats": [[base stat, [stat name]],...]}}
 var pokemonId = 1;
 
 window.onload = async function() {
@@ -14,7 +14,6 @@ window.onload = async function() {
         pokemon.addEventListener("click", updatePokemon);
         document.getElementById("pokemon-list").append(pokemon);
     }
-    console.log(pokedex);
     document.getElementById("pokemon-description").innerText = pokedex[1]["desc"];
 }
 
@@ -28,14 +27,14 @@ async function getPokemon(num) {
     let pokemonType = pokemon["types"];
     let pokemonImg = pokemon["sprites"]["front_default"];
     let pokemonShinyImg = pokemon["sprites"]["front_shiny"];
+    let pokemonStats = pokemon["stats"];
 
     res = await fetch(pokemon["species"]["url"]);
     let pokemonDesc = await res.json();
 
     //Get the pokemon description
     pokemonDesc = pokemonDesc["flavor_text_entries"][9]["flavor_text"].replace(/\n/g, " ");
-    pokedex[num] = {"name": pokemonName, "img": pokemonImg, "types": pokemonType, "desc": pokemonDesc, "shinyImg": pokemonShinyImg};
-    console.log(pokemon);
+    pokedex[num] = {"name": pokemonName, "img": pokemonImg, "types": pokemonType, "desc": pokemonDesc, "shinyImg": pokemonShinyImg, "stats": pokemonStats};
 }
 
 function updatePokemon() {
@@ -60,6 +59,14 @@ function updatePokemon() {
 
     //Update pokemon description
     document.getElementById("pokemon-description").innerText = pokedex[this.id]["desc"];
+
+    //Update pokemon stats
+    document.getElementById("hp-stat").innerText = pokedex[this.id]["stats"][0]["base_stat"]
+    document.getElementById("attack-stat").innerText = pokedex[this.id]["stats"][1]["base_stat"]
+    document.getElementById("defense-stat").innerText = pokedex[this.id]["stats"][2]["base_stat"]
+    document.getElementById("spatk-stat").innerText = pokedex[this.id]["stats"][3]["base_stat"]
+    document.getElementById("spdef-stat").innerText = pokedex[this.id]["stats"][4]["base_stat"]
+    document.getElementById("speed-stat").innerText = pokedex[this.id]["stats"][5]["base_stat"]
 }
 
 function updateShinySprite() {
